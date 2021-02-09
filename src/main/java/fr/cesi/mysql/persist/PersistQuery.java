1b279eae169db8781fccf818b242d53e32dea9cb
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-02-09 11:38:36 +0100

    Complete rework of PersistQuery

--- src/main/java/fr/cesi/mysql/persist/PersistQuery.java
+++ src/main/java/fr/cesi/mysql/persist/PersistQuery.java
@@ -1,173 +1,145 @@
 package fr.cesi.mysql.persist;
 
+import fr.cesi.meteo.domain.model.Data;
+import fr.cesi.mysql.Values;
 import fr.cesi.mysql.connector.SQLConnectionAdapter;
 import fr.cesi.mysql.connector.SQLConnectionAdapterFactory;
-import fr.cesi.mysql.persist.annotation.Key;
-import fr.cesi.mysql.persist.exception.PersistQueryException;
-import fr.cesi.mysql.utils.ReflectionUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 
 import java.lang.reflect.Field;
 import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
+@RequiredArgsConstructor
 public class PersistQuery<T extends Persist> {
-
     private final Class<? extends Persist> clazz;
 
-    public PersistQuery(Class<? extends Persist> type) {
-        this.clazz = type;
+    private String query = "";
+    private Object[] objectsForStatement = new Object[0];
+
+    public PersistQuery<T> select(Persist persist) {
+        HashMap<String, Object> persistData = getPersistData(persist);
+
+        return select(persistData.keySet().toArray(new String[0]))
+                .where(persistData);
     }
 
-    public List<T> findAll() {
-        return find(null);
-    }
-    
-    public T getOrCreate(Persist persist) {
-        List<T> ts = find(persist);
-
-        if (ts.size() > 1)
-            try {
-                throw new PersistQueryException("More than one " + persist.getClass().getSimpleName() + " founded!");
-            } catch (PersistQueryException e) {
-                e.printStackTrace();
-            }
-        else if (ts.size() == 0) {
-            create(persist);
-            persist = find(persist).get(0);
-        } else
-            persist = ts.get(0);
-
-        return (T) persist;
-    }
-
-    public boolean create(Persist persist) {
-        Optional<SQLConnectionAdapter> sqlConnectionAdapter = SQLConnectionAdapterFactory
-                .getInstance().getSQLConnectionAdapter(clazz);
-        HashMap<String, Object> declaredFields = new HashMap<>(ReflectionUtils.getDeclaredFields(persist, persist.getClass()));
-        String query = String.format(
-                "INSERT INTO %s (%s) VALUES (%s)",
-                persist.getTable(),
-                String.join(",", declaredFields.keySet()),
-                declaredFields.values().stream().map(o -> "?").collect(Collectors.joining(","))
+    public PersistQuery<T> select(String... fields) {
+        String table = Persist.getTable(Data.class);
+        query += String.format(
+                "SELECT %s FROM %s ",
+                String.join(",", fields),
+                Persist.getTable(Data.class)
         );
-
-        if (sqlConnectionAdapter.isPresent())
-            return sqlConnectionAdapter.get().update(query, declaredFields.values().toArray(new Object[0])) > 0;
-        return false;
+        return this;
     }
 
-    public boolean save(Persist persist) {
-        Optional<SQLConnectionAdapter> sqlConnectionAdapter = SQLConnectionAdapterFactory
-                .getInstance().getSQLConnectionAdapter(clazz);
-        HashMap<String, Object> declaredFields = new HashMap<>(ReflectionUtils.getDeclaredFields(persist, persist.getClass()));
-        String query = "UPDATE `" + persist.getTable() + "` SET ";
-
-        declaredFields.remove("id");
-        query += declaredFields.keySet().stream().map(key -> "`" + key + "`=?").collect(Collectors.joining(", "));
-        query += " WHERE `id`=" + persist.getId().intValue();
-
-        if (sqlConnectionAdapter.isPresent())
-            return sqlConnectionAdapter.get().update(query, declaredFields.values().toArray(new Object[0])) > 0;
-        return false;
+    public PersistQuery<T> update(Persist persist) {
+        HashMap<String, Object> persistData = getPersistData(persist);
+        return update(persistData).where(persistData);
     }
 
-    public List<T> find(Persist persist) {
+    public PersistQuery<T> update(Map<String, Object> fields) {
+        String table = Persist.getTable(clazz);
+
+        query += String.format(
+                "UPDATE %s SET %s",
+                table,
+                String.join("=?, ", fields.keySet())
+        );
+        objectsForStatement = fields.values().toArray(new Object[0]);
+
+        return this;
+    }
+
+    public PersistQuery<T> insert(Persist persist) {
+        return insert(getPersistData(persist));
+    }
+
+    public PersistQuery<T> insert(Map<String, Object> fields) {
+        String table = Persist.getTable(Data.class);
+        query += String.format(
+                "INSERT INTO %s (%s) VALUES (%s)",
+                table,
+                String.join(", ", fields.keySet()),
+                fields.values().stream().map(o -> "?").collect(Collectors.joining(", "))
+        );
+        objectsForStatement = fields.values().toArray(new Object[0]);
+        return this;
+    }
+
+    public PersistQuery<T> where(HashMap<String, Object> where) {
+        query += "WHERE ";
+
+        ((HashMap<String, Object>) where.clone()).forEach((s, o) -> {
+            if (o == null || Integer.parseInt(String.valueOf(o)) == Values.NULL_INTEGER) {
+                where.remove(s);
+            }
+        });
+
+        for (String s : where.keySet())
+            query += s + "=? AND ";
+        objectsForStatement = where.values().toArray(new Object[0]);
+
+        query = query.substring(0, query.length()-"AND ".length());
+        return this;
+    }
+
+    public PersistQuery<T> orderBy(String order) {
+        query += "ORDER BY " + order;
+        return this;
+    }
+
+    @SneakyThrows
+    public List<T> executeQuery() {
         List<T> persists = new ArrayList<>();
-        Optional<SQLConnectionAdapter> sqlConnectionAdapter = SQLConnectionAdapterFactory
-                .getInstance().getSQLConnectionAdapter(clazz);
-        HashMap<String, Object> declaredFields = new HashMap<>();
-        String whereClause = "";
+        Optional<SQLConnectionAdapter> optionalSQLConnectionAdapter = SQLConnectionAdapterFactory
+                .getInstance()
+                .getSQLConnectionAdapter(Data.class);
 
-        if(persist != null) {
-            declaredFields.putAll(ReflectionUtils.getDeclaredFields(persist, persist.getClass()));
-            whereClause = "WHERE " + String.join("=? AND ", declaredFields.keySet()) + "=?";
-        }
+        if (optionalSQLConnectionAdapter.isPresent()) {
+            SQLConnectionAdapter sqlConnectionAdapter = optionalSQLConnectionAdapter.get();
 
-        if (sqlConnectionAdapter.isPresent()) {
-            Optional<ResultSet> resultSet = sqlConnectionAdapter.get().query(String.format(
-                    "SELECT * FROM %s %s",
-                    Persist.getTable(clazz),
-                    whereClause
-            ), declaredFields.values().toArray(new Object[0]));
+            Optional<ResultSet> optionalResultSet = sqlConnectionAdapter.query(query, objectsForStatement);
 
-            if (resultSet.isPresent()) {
-                try {
-                    while (resultSet.get().next()) {
-                        T persistType = (T) clazz.newInstance().parseFromResultSet(resultSet.get());
-                        persists.add(persistType);
-                    }
-                } catch (SQLException | InstantiationException | IllegalAccessException throwable) {
-                    throwable.printStackTrace();
+            if (optionalResultSet.isPresent()) {
+                ResultSet resultSet = optionalResultSet.get();
+                while (resultSet.next()) {
+                    T persistType = (T) clazz.newInstance().parseFromResultSet(resultSet);
+                    persists.add(persistType);
                 }
             }
         }
+
         return persists;
     }
 
-    public int delete(Persist persist) {
+    public int executeUpdate() {
         Optional<SQLConnectionAdapter> sqlConnectionAdapter = SQLConnectionAdapterFactory
-                .getInstance().getSQLConnectionAdapter(clazz);
-        HashMap<String, Object> declaredFields = new HashMap<>();
-        String whereClause = "";
+                .getInstance()
+                .getSQLConnectionAdapter(Data.class);
 
-        if(persist != null) {
-            declaredFields.putAll(ReflectionUtils.getDeclaredFields(persist, persist.getClass()));
-            whereClause = "WHERE " + String.join("=? AND ", declaredFields.keySet()) + "=?";
-        }
-
-        if (sqlConnectionAdapter.isPresent()) {
-            return sqlConnectionAdapter.get().update(String.format(
-                    "DELETE FROM %s %s",
-                    Persist.getTable(clazz),
-                    whereClause
-            ), declaredFields.values().toArray(new Object[0]));
-        } else return 0;
+        return sqlConnectionAdapter.map(connectionAdapter -> connectionAdapter.update(query, objectsForStatement)).orElse(0);
     }
 
-    public int createTable() {
-        Optional<SQLConnectionAdapter> sqlConnectionAdapter = SQLConnectionAdapterFactory
-                .getInstance().getSQLConnectionAdapter(clazz);
-        StringBuilder createTableQuery = new StringBuilder(
-                String.format("CREATE TABLE IF NOT EXISTS `%s` (", Persist.getTable(clazz))
-        );
 
-        List<Field> fields = ReflectionUtils.getDeclaredFields(clazz);
-        fields = fields.stream().filter(field -> field.getAnnotation(Key.class) != null).collect(Collectors.toList());
+    @SneakyThrows
+    private HashMap<String, Object> getPersistData(Persist persist) {
+        final HashMap<String, Object> persistData = new HashMap<>();
+        Class<?>[] classes = new Class<?>[] {
+                clazz.getSuperclass(),
+                clazz
+        };
 
-        for (Field field : fields) {
-            Key key = field.getAnnotation(Key.class);
-            String fieldQuery = "`" + field.getName() + "` " + key.keyType().name().toLowerCase();
-
-            if (key.length() != -1)
-                fieldQuery += "(" + key.length() + ")";
-
-            if (key.unsigned())
-                fieldQuery += " UNSIGNED";
-
-            if (key.notNull())
-                fieldQuery += " NOT NULL";
-
-            if (key.autoincrement())
-                fieldQuery += " AUTO_INCREMENT";
-
-            fieldQuery += ", ";
-            createTableQuery.append(fieldQuery);
-
-            if (key.autoincrement())
-                createTableQuery.append("KEY(").append(field.getName()).append("), ");
+        for (Class<?> aClass : classes) {
+            for (Field declaredField : aClass.getDeclaredFields()) {
+                declaredField.setAccessible(true);
+                persistData.put(declaredField.getName(), declaredField.get(persist));
+            }
         }
 
-        createTableQuery
-                .replace(createTableQuery.length()-", ".length(), createTableQuery.length(), "")
-                .append(");");
-
-        return sqlConnectionAdapter.map(connectionAdapter -> connectionAdapter.update(createTableQuery.toString()))
-                .orElse(0);
+        return persistData;
     }
-
 }
