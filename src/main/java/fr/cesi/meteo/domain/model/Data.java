5ada2389e181b47a85f76d08c775356773d600ad
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-02-09 11:39:10 +0100

    Add implementation of null integer values

--- src/main/java/fr/cesi/meteo/domain/model/Data.java
+++ src/main/java/fr/cesi/meteo/domain/model/Data.java
@@ -1,27 +1,28 @@
 package fr.cesi.meteo.domain.model;
 
+import fr.cesi.mysql.Values;
 import fr.cesi.mysql.persist.Persist;
 import fr.cesi.mysql.persist.annotation.Key;
 import fr.cesi.mysql.persist.annotation.KeyType;
 import fr.cesi.mysql.persist.annotation.Table;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
-@Table(name = "datas")
-@Builder
 @Getter
 @Setter
+@ToString
+@Table(name = "datas")
 public class Data extends Persist {
 
     @Key(keyType = KeyType.BIGINT, notNull = true)
-    private long createdAt;
+    private long createdAt = Values.NULL_INTEGER;
 
     @Key(keyType = KeyType.INT, notNull = true)
-    private int temperature;
+    private int temperature = Values.NULL_INTEGER;
 
     @Key(keyType = KeyType.INT, notNull = true, unsigned = true)
-    private int humidity;
-
+    private long humidity = Values.NULL_INTEGER;
 
 }
+
