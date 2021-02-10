c0b33d944521f4d30581178a6529a1c63d830016
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-02-10 10:33:53 +0100

    Add find by id method

--- src/main/java/fr/cesi/meteo/application/repository/DataRepository.java
+++ src/main/java/fr/cesi/meteo/application/repository/DataRepository.java
@@ -4,6 +4,9 @@ import fr.cesi.meteo.domain.entity.Data;
 import fr.cesi.divers.mysql.persist.PersistQuery;
 import fr.cesi.meteo.domain.repository.IDataRepository;
 
+import java.math.BigInteger;
+import java.util.HashMap;
+
 public class DataRepository implements IDataRepository {
 
     @Override
@@ -21,5 +24,16 @@ public class DataRepository implements IDataRepository {
         return new PersistQuery<>(Data.class)
                 .insert(data)
                 .executeUpdate();
+    }
+
+    @Override
+    public Data find(int id) {
+        return (Data) new PersistQuery<>(Data.class)
+                .select("*")
+                .where(new HashMap<String, Object>() {{
+                    put("id", id);
+                }})
+                .executeQuery()
+                .get(0);
     }
 }
