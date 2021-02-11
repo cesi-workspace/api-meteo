17b9a840ff1ee0e904f245b9a4559587181ce495
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-02-11 09:53:28 +0100

    Add find api key by key

--- src/main/java/fr/cesi/meteo/application/repository/ApiKeyRepository.java
+++ src/main/java/fr/cesi/meteo/application/repository/ApiKeyRepository.java
@@ -1,6 +1,26 @@
 package fr.cesi.meteo.application.repository;
 
+import fr.cesi.divers.mysql.persist.Persist;
+import fr.cesi.divers.mysql.persist.PersistQuery;
+import fr.cesi.meteo.domain.entity.ApiKey;
 import fr.cesi.meteo.domain.repository.IApiKeyRepository;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class ApiKeyRepository implements IApiKeyRepository {
+
+    @Override
+    public ApiKey findByKey(String token) {
+        List<Persist> apiKeys = new PersistQuery<>(ApiKey.class)
+                .select("*")
+                .where(new HashMap<String, Object>() {{
+                    put("key", token);
+                }})
+                .executeQuery();
+
+        if (apiKeys.size() > 0)
+            return (ApiKey) apiKeys.get(0);
+        return null;
+    }
 }
--- src/main/java/fr/cesi/meteo/domain/repository/IApiKeyRepository.java
+++ src/main/java/fr/cesi/meteo/domain/repository/IApiKeyRepository.java
@@ -1,4 +1,7 @@
 package fr.cesi.meteo.domain.repository;
 
+import fr.cesi.meteo.domain.entity.ApiKey;
+
 public interface IApiKeyRepository {
+    ApiKey findByKey(String token);
 }
