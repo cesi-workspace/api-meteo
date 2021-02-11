587c7e3238eb86081d4f85bf1939d31130b9bc07
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-02-11 09:54:28 +0100

    Add verify api token method

--- src/main/java/fr/cesi/meteo/application/service/ApiKeyService.java
+++ src/main/java/fr/cesi/meteo/application/service/ApiKeyService.java
@@ -1,5 +1,8 @@
 package fr.cesi.meteo.application.service;
 
+import fr.cesi.meteo.configuration.factory.RepositoryFactory;
+import fr.cesi.meteo.domain.entity.ApiKey;
+import fr.cesi.meteo.domain.repository.IApiKeyRepository;
 import fr.cesi.meteo.domain.service.IApiKeyService;
 import fr.cesi.meteo.infrastructure.http.Request;
 
@@ -7,7 +10,17 @@ public class ApiKeyService implements IApiKeyService {
 
     @Override
     public boolean verifyApiToken(Request request) {
-        return false;
+        IApiKeyRepository apiKeyRepository = RepositoryFactory.getInstance().getApiKeyRepository();
+        String token = null;
+
+        if (request.getHeaders().containsKey("Authorization"))
+            token = request.getHeaders().getFirst("Authorization");
+        else
+            return false;
+
+        ApiKey apiKey = apiKeyRepository.findByKey(token);
+
+        return apiKey != null;
     }
 
 }
