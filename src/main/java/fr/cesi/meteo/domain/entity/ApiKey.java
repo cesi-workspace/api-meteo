29d5a0beb2d9b33dd5249f25780a55b6105fe280
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-02-11 09:55:04 +0100

    Add extends persist, for PersistQuery

--- src/main/java/fr/cesi/meteo/domain/entity/ApiKey.java
+++ src/main/java/fr/cesi/meteo/domain/entity/ApiKey.java
@@ -1,5 +1,6 @@
 package fr.cesi.meteo.domain.entity;
 
+import fr.cesi.divers.mysql.persist.Persist;
 import fr.cesi.divers.mysql.persist.annotation.Key;
 import fr.cesi.divers.mysql.persist.annotation.KeyType;
 import fr.cesi.divers.mysql.persist.annotation.Table;
@@ -11,7 +12,7 @@ import lombok.ToString;
 @Setter
 @ToString
 @Table(name = "api_keys")
-public class ApiKey {
+public class ApiKey extends Persist {
 
     @Key(keyType = KeyType.VARCHAR, notNull = true, length = 255)
     private String key;
